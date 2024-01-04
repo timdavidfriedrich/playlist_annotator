ff6ce45949b6d2e6624ad1bdde7649bf10639fc1
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2024-01-04 17:30:12 +0100

    [CHANGE] DataService's file name to match class name

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:playlist_annotator/config/themes/theme_config.dart';
 import 'package:playlist_annotator/features/core/localization/messages.dart';
 import 'package:playlist_annotator/features/core/services/local_storage_services.dart';
 import 'package:playlist_annotator/features/core/services/auth_service.dart';
-import 'package:playlist_annotator/features/core/services/database_service.dart';
+import 'package:playlist_annotator/features/core/services/data_service.dart';
 import 'package:playlist_annotator/features/core/services/pocketbase_service.dart';
 import 'package:playlist_annotator/features/core/services/spotify_service.dart';
 import 'package:playlist_annotator/features/core/services/user_service.dart';
