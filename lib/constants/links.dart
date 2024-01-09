85c236c2bf143593988fba08e3a683fe7f706c25
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2024-01-09 21:09:30 +0100

    [ADD] Real database link

--- lib/constants/links.dart
+++ lib/constants/links.dart
@@ -1,7 +1,8 @@
 class Links {
   Links._();
 
-  static const String pocketbaseUrl = "http://192.168.1.221:8090";
+  static const String pocketbaseUrl = "http://130.61.245.138:8090";
   static const String spotifyBaseUrl = "https://api.spotify.com/v1";
   static const String spotifyTokenUrl = "https://accounts.spotify.com/api/token";
+  static const String spotifyPlaylistUrl = "https://open.spotify.com/playlist";
 }
