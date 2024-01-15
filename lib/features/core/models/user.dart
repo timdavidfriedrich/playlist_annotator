846b03134a8faad3a13ef74f4a52e777340576f8
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2024-01-15 20:27:56 +0100

    [CHANGE] name, email, avatarUrl to optional

--- lib/features/core/models/user.dart
+++ lib/features/core/models/user.dart
@@ -1,11 +1,12 @@
+import 'package:get/get_utils/get_utils.dart';
 import 'package:pocketbase/pocketbase.dart';
 
 class User {
   final String id;
   final String spotifyId;
   final String name;
-  final String email;
-  final String avatarUrl;
+  final String? email;
+  final String? avatarUrl;
 
   User({
     required this.id,
@@ -18,7 +19,7 @@ class User {
   User.fromPocketbaseRecord(RecordModel record)
       : id = record.id,
         spotifyId = record.data["username"],
-        name = record.data["name"],
+        name = record.data["name"] ?? "unnamed_label".tr,
         email = record.data["email"],
         avatarUrl = record.data["avatarUrl"];
 }
