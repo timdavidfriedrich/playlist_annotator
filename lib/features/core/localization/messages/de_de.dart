be5035d5da08d90b92476f9eb2b45791a3ecff98
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2024-01-17 13:13:44 +0100

    [ADD] message grouping

--- lib/features/core/localization/messages/de_de.dart
+++ lib/features/core/localization/messages/de_de.dart
@@ -1,26 +1,40 @@
 const Map<String, String> deDE = {
+  // App
   "app_title": "Playlist Annotator",
   "test_version_label": "(Alpha-Test)",
+
+  // Onboarding
+  "sign_in_spotify_label": "Mit Spotify anmelden",
+
+  // Error
   "error_label": "Fehler",
+  "unknown_error": "Unbekannter Fehler.",
+  "playlist_not_found_error": "Playlist nicht gefunden.",
+  "playlist_already_joined_error": "Du bist dieser Playlist bereits beigetreten.",
+  "spotify_playlist_not_found_error": "Spotify-Playlist nicht gefunden.",
+  "playlist_already_added_error": "Diese Playlist wurde bereits hinzugefügt.",
+
+  // Actions
   "cancel_label": "Abbrechen",
+  "close_label": "Schließen",
   "save_label": "Speichern",
-  "ok_label": "Okay",
-  "empty_home_greeting": "Willkommen @name!\nFüge deine erste Playlist hinzu.",
-  "settings_label": "Einstellungen",
-  "linked_account_label": "Verknüpftes Konto",
-  "credits_label": "Credits",
-  "add_playlist_label": "Playlist hinzufügen",
+  "okay_label": "Okay",
   "remove_label": "Entfernen",
+
+  // Home
+  "empty_home_greeting": "Willkommen @name!\nFüge deine erste Playlist hinzu.",
+  "add_playlist_label": "Playlist hinzufügen",
   "remove_playlist_label": "Playlist entfernen",
-  "annotator_list_label": "Annotatoren",
-  "share_my_global_annotations_label": "Meine globalen Anmerkungen teilen",
-  "share_my_global_annotations_description":
-      "Wenn aktiviert, können andere Playlist-Annotatoren deine globalen Song-Anmerkungen sehen - aber nur für diese Playlist.",
   "keep_annotations_after_deletion_label": "Anmerkungen behalten",
   "keep_annotations_after_deletion_description":
       "Wenn aktiviert, werden die Anmerkungen noch da sein, wenn du diese Playlist erneut hinzufügst.",
-  "playlist_uri_label": "Playlist URI",
-  "sign_in_spotify_label": "Mit Spotify anmelden",
+
+  // Settings
+  "settings_label": "Einstellungen",
+  "linked_account_label": "Verknüpftes Konto",
+  "credits_label": "Credits",
+
+  // Playlist and songs
   "unnamed_label": "Unbenannt",
   "unknown_author_label": "Unbekannter Autor",
   "song_count_label": "@count Songs",
@@ -31,4 +45,16 @@ const Map<String, String> deDE = {
   "add_comment_label": "Kommentar hinzufügen",
   "add_first_comment_label": "Ersten Kommentar hinzufügen",
   "open_in_spotify": "In Spotify öffnen",
+
+  // Playlist options
+  "annotator_list_label": "Annotatoren",
+  "share_label": "Einladen",
+  "share_playlist_label": "Jemanden zur Playlist einladen",
+  "share_code_label": "Code",
+  "playlist_code_has_been_copied_label": "Einladungscode wurde in die Zwischenablage kopiert.",
+  "add_playlist_from_code_label": "Einladungscode",
+  "add_playlist_from_code_error": "Ungültiger Code",
+  "share_my_global_annotations_label": "Meine globalen Anmerkungen teilen",
+  "share_my_global_annotations_description":
+      "Wenn aktiviert, können andere Playlist-Annotatoren deine globalen Song-Anmerkungen sehen - aber nur für diese Playlist.",
 };
--- lib/features/core/localization/messages/en_us.dart
+++ lib/features/core/localization/messages/en_us.dart
@@ -1,33 +1,59 @@
 const Map<String, String> enUS = {
+  // App
   "app_title": "Playlist Annotator",
-  "test_version_label": "(alpha test)",
+  "test_version_label": "(Alpha Test)",
+
+  // Onboarding
+  "sign_in_spotify_label": "Sign in with Spotify",
+
+  // Error
   "error_label": "Error",
+  "unknown_error": "Unknown error.",
+  "playlist_not_found_error": "Playlist not found.",
+  "playlist_already_joined_error": "You already joined this playlist.",
+  "spotify_playlist_not_found_error": "Spotify playlist not found.",
+  "playlist_already_added_error": "This playlist has already been added.",
+
+  // Actions
   "cancel_label": "Cancel",
+  "close_label": "Close",
   "save_label": "Save",
-  "ok_label": "Okay",
+  "okay_label": "Okay",
+  "remove_label": "Remove",
+
+  // Home
   "empty_home_greeting": "Welcome @name!\nAdd your first playlist.",
+  "add_playlist_label": "Add playlist",
+  "remove_playlist_label": "Remove playlist",
+  "keep_annotations_after_deletion_label": "Keep annotations",
+  "keep_annotations_after_deletion_description": "If enabled, the annotations will still be there if you add this playlist again.",
+
+  // Settings
   "settings_label": "Settings",
   "linked_account_label": "Linked account",
   "credits_label": "Credits",
-  "add_playlist_label": "Add playlist",
-  "remove_label": "Remove",
-  "remove_playlist_label": "Remove playlist",
-  "annotator_list_label": "Annotators",
-  "share_my_global_annotations_label": "Share my global annotations",
-  "share_my_global_annotations_description":
-      "If enabled, other playlist annotators can view your global song annotations - but only for this playlist.",
-  "keep_annotations_after_deletion_label": "Keep annotations",
-  "keep_annotations_after_deletion_description": "If enabled, the annotations will be still there if you add this playlist, again.",
-  "playlist_uri_label": "Playlist URI",
-  "sign_in_spotify_label": "Sign in with Spotify",
+
+  // Playlist and songs
   "unnamed_label": "Unnamed",
   "unknown_author_label": "Unknown author",
   "song_count_label": "@count songs",
   "mark_all_as_read_label": "Mark all as read",
   "collapse_all_label": "Collapse all",
   "expand_all_label": "Expand all",
-  "no_annotations_label": "No annotations, yet.",
-  "add_comment_label": "Add a comment",
-  "add_first_comment_label": "Add the first comment",
+  "no_annotations_label": "No annotations yet.",
+  "add_comment_label": "Add comment",
+  "add_first_comment_label": "Add first comment",
   "open_in_spotify": "Open in Spotify",
+
+  // Playlist options
+  "annotator_list_label": "Annotators",
+  "share_label": "Invite",
+  "share_playlist_label": "Invite someone to the playlist",
+  "share_code_label": "Code",
+  "playlist_code_has_been_copied_label": "Invitation code has been copied to the clipboard.",
+  "add_playlist_from_code_label": "Invitation code",
+  "add_playlist_from_code_error": "Invalid code",
+  "share_my_global_annotations_label": "Share my global annotations",
+  "share_my_global_annotations_description":
+      "If enabled, other playlist annotators can see your global song annotations - but only for this playlist.",
 };
