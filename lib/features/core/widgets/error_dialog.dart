304316911e40adb26d61695e6258b6df66bd21d4
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2024-01-17 13:15:34 +0100

    [CHANGE] Make exception optional

--- lib/features/core/widgets/error_dialog.dart
+++ lib/features/core/widgets/error_dialog.dart
@@ -3,14 +3,14 @@ import 'package:get/get.dart';
 import 'package:playlist_annotator/features/core/resources/exceptions/app_exception.dart';
 
 class ErrorDialog extends StatelessWidget {
-  final AppException exception;
-  const ErrorDialog({super.key, required this.exception});
+  final AppException? exception;
+  const ErrorDialog({super.key, this.exception});
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text("error_label".tr),
-      content: Text(exception.message ?? "unknown_error".tr),
+      content: Text(exception?.message ?? "unknown_error".tr),
       actions: [
         TextButton(
           onPressed: () => Get.back(),
